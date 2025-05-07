3b5792ae160d7d6c5f47254c6ca5b3407041ead6
sahanpramudith <pramuditsahan466@gmail.com>
2025-05-07 13:04:09 +0530

    add Patient class with attributes and Lombok annotations

---
+++ src/main/java/edu/icet/orm/dto/Patient.java
@@ -0,0 +1,27 @@
+package edu.icet.orm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor
+@Data
+public class Patient {
+
+    private Integer id;
+    private String name;
+    private String nic;
+    private String address;
+    private String bloodGroup;
+    private String category;
+    private String gender;
+    private String contact;
+    private String note;
+    private String age;
+    private String allergis;
+
+
+}
